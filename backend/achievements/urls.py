035671b862978a090c11f86cb339043fd6bead76
Aayush Gautam <aayu.gautam@gmail.com>
2025-11-13 15:27:22 +0530

    Update urls.py

--- backend/achievements/urls.py
+++ backend/achievements/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AchievementViewSet, NotificationViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'list', AchievementViewSet, basename='achievement')
@@ -8,4 +9,6 @@ router.register(r'notifications', NotificationViewSet, basename='notification')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', views.custom_login, name='login'),
+    path('create-admin/', views.create_admin_user, name='create_admin'),
 ]
