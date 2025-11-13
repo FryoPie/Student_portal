8b65b8b6859223fd572969f79f6c0fbff314cd0c
Aayush Gautam <aayu.gautam@gmail.com>
2025-11-13 09:42:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
   server: {
     port: 5173,
     proxy: {
       '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/media': {
         target: process.env.VITE_API_URL || 'http://localhost:8000',
         changeOrigin: true,
       }
